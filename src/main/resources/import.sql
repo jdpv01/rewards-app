304434353754cb8800f0195acd05b321b82c0344
jdpv01 <juandpv01@hotmail.com>
2022-12-12 15:49:38 -0500

    added demo imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,8 +5,11 @@ INSERT INTO "role" (name) VALUES('ROLE_ADMIN')
 INSERT INTO "store" (id, name, image) VALUES ('a053803a-1181-4f91-855c-74841d7a2c0c', 'Éxito', null)
 INSERT INTO "store" (id, name, image) VALUES ('1e7232f0-6ad9-4e87-84db-f10f4501bac2', 'Jumbo', null)
 
-INSERT INTO "promotion" (id, name, content, image, offered_points, available_quantity, validity_date) VALUES ('5e21a137-adfa-4c00-a411-e3c13f18ed7c', 'banano', 'Banano cualquier peso precio mínimo $2000', null, 100, 80, null)
-INSERT INTO "promotion" (id, name, content, image, offered_points, available_quantity, validity_date) VALUES ('606ab345-2af3-4621-b315-7e8a1a878bd2', 'cebolla', 'Cebolla cualquier peso precio mínimo $2000', null, 150, 50, null)
+INSERT INTO "promotion" (id, name, content, image, offered_points, available_quantity, validity_date) VALUES ('5e21a137-adfa-4c00-a411-e3c13f18ed7c', 'CREMA COLGATE', 'Crema Colgate triple acción 225ml', null, 100, 80, null)
+INSERT INTO "promotion" (id, name, content, image, offered_points, available_quantity, validity_date) VALUES ('606ab345-2af3-4621-b315-7e8a1a878bd2', 'Jamón Zenú', 'Jamón Sánduche Zenú x 400 gr', null, 150, 50, null)
+INSERT INTO "promotion" (id, name, content, image, offered_points, available_quantity, validity_date) VALUES ('7db8c288-eb5b-4190-a1a3-aa4bc8f5a8a3', 'Jamón de cerdo', 'JAMON DE CERDO SIN CONSERV PIETRAN 431 gr', null, 200, 10, null)
+INSERT INTO "promotion" (id, name, content, image, offered_points, available_quantity, validity_date) VALUES ('bfb7f41d-eb6e-4e90-a37e-e073a9062f57', 'SALCHICHAS', 'SALCHICHA RANCHERA 480 Gramo', null, 100, 36, null)
+INSERT INTO "promotion" (id, name, content, image, offered_points, available_quantity, validity_date) VALUES ('99d141d2-1521-4289-8b38-21257592d672', 'Crema Axión', 'LAVALOZA LIMÓN CREMA AXION', null, 50, 40, null)
 
 INSERT INTO "reward" (id, description, link, code, image, available_quantity, required_points, validity_date) VALUES ('4987a740-8564-4fde-b349-af39a9d2309c', 'Éxito Bono Virtual $50.000', 'https://tubono.com/redimir-efectivo-bono-virtual-exito/?v=42983b05e2f2', 'MePYAWhXok', null, '100', 100, null)
 
